d74ee3f5009cad613af10cfa52569ef895a60c4b
Jason R. Surratt <jason.surratt@gmail.com>
2021-08-22 21:17:11 -0600

    Add DeleteChildren to transform

--- Runtime/UnityExtensions/TransformExtensions.cs
+++ Runtime/UnityExtensions/TransformExtensions.cs
@@ -4,6 +4,20 @@ namespace Plucky.UnityExtensions
 {
     public static class TransformExtensions
     {
+        public static void DestroyChildren(this Transform transform)
+        {
+            int watchdog = transform.childCount * 2 + 10;
+            while (transform.childCount > 0)
+            {
+                GameObject.DestroyImmediate(transform.GetChild(0).gameObject);
+                if (watchdog-- <= 0)
+                {
+                    Debug.Log(transform.childCount);
+                    Debug.LogWarning("Failed to delete all children.");
+                    return;
+                }
+            }
+        }
 
         public static string GetPath(this Transform current)
         {
