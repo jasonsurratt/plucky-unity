5b527d5dbdb4fb9d72c9a4b42f7d847ca6121db0
Jason R. Surratt <jason.surratt@gmail.com>
2021-09-05 15:30:01 -0600

    Add transform child sorting

---
+++ Editor/Utils/TransformChildSorterEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Plucky.Unity
+{
+    [CustomEditor(typeof(TransformChildSorter))]
+    public class TransformChildSorterEditor : Editor
+    {
+        [MenuItem("GameObject/Knockback/Sort Children", false, 49)]
+        public static void GridifyChildren(MenuCommand cmd)
+        {
+            GameObject go = (GameObject)cmd.context;
+            TransformChildSorter.Sort(go.transform);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            TransformChildSorter t = (TransformChildSorter)target;
+            if (GUILayout.Button("Sort"))
+            {
+                t.Apply();
+            }
+        }
+    }
+}
---
+++ Runtime/Utils/TransformChildSorter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Plucky.Unity
+{
+    /// TransformChildSorter sorts the children of this object by name. This fixes the arbitrary
+    /// re-ordering of children that sometimes happens in the editor.
+    public class TransformChildSorter : MonoBehaviour
+    {
+        class Comparer : IComparer<Transform>
+        {
+            public int Compare(Transform a, Transform b)
+            {
+                return a.name.CompareTo(b.name);
+            }
+        }
+
+        public void Apply()
+        {
+            Sort(transform);
+        }
+
+        public static void Sort(Transform target)
+        {
+            // use this craziness to get RectTransform and Transform.
+            var children = target.gameObject.GetComponentsInChildren<Transform>().
+                Where(x => x.parent == target).ToList();
+            children.Sort(new Comparer());
+
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].SetSiblingIndex(i);
+            }
+        }
+
+        void Awake()
+        {
+            Apply();
+        }
+    }
+}
