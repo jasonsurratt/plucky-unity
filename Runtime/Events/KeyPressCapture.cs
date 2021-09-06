310492041a465c7104f7883ff5bd992382d661b9
Jason R. Surratt <jason.surratt@gmail.com>
2021-09-06 12:45:50 -0600

    Add key press capture

---
+++ Runtime/Events/KeyPressCapture.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Plucky.Unity
+{
+    public class KeyPressCapture : MonoBehaviour
+    {
+        public UnityEvent keyPressEvent;
+        public KeyCode keyCode;
+
+        protected void Update()
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                keyPressEvent.Invoke();
+            }
+        }
+    }
+}
