317de46d08bfe8bcc558d637cb0f1edd78397389
Jason R. Surratt <jason.surratt@gmail.com>
2021-09-06 12:26:24 -0600

    Add toggle gameobjec tfucntion

---
+++ Runtime/Utils/ToggleActive.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plucky.Unity
+{
+    public class ToggleActive : MonoBehaviour
+    {
+        public void Toggle()
+        {
+            if (gameObject.activeSelf)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
