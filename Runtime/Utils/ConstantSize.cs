dfc23a1e07fb6d395313ae663b008f64affac223
Jason R. Surratt <jason.surratt@gmail.com>
2021-09-15 16:32:38 -0600

    Add script for maintaining a constant object size

---
+++ Runtime/Utils/ConstantSize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Plucky.Unity
+{
+    /// <summary>
+    /// Keep the transform a constant size regardless of the parent transform.
+    /// </summary>
+    public class ConstantSize : MonoBehaviour
+    {
+        public float initialScale = -1;
+
+        // Use this for initialization
+        protected void Start()
+        {
+            if (initialScale <= 0) initialScale = transform.lossyScale.x;
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+            float scale = transform.lossyScale.x;
+            transform.localScale *= initialScale / scale;
+        }
+    }
+}
