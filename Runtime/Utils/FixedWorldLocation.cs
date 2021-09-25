0b2a2e01c3e74c007ce821a8260e41c70f2bf4a0
Jason R. Surratt <jason.surratt@gmail.com>
2021-09-24 21:20:38 -0600

    Add simple script to keep object in place

---
+++ Runtime/Utils/FixedWorldLocation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace docker
+{
+    /// <summary>
+    /// FixedWorldLocation forces this object to a fixed world location every frame.
+    /// </summary>
+    public class FixedWorldLocation : MonoBehaviour
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+
+        protected void Update()
+        {
+            transform.position = position;
+            transform.eulerAngles = rotation;
+        }
+    }
+}
